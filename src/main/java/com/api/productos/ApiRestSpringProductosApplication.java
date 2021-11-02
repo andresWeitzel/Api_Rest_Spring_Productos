fa854d078ae5c15c095419d2b0865707aa8466e4
andres <andres06energy@hotmail.com>
2021-11-01 21:50:57 -0300

    Created Spring Boot Proyect

---
+++ src/main/java/com/api/productos/ApiRestSpringProductosApplication.java
@@ -0,0 +1,13 @@
+package com.api.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/productos/ApiRestSpringProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
