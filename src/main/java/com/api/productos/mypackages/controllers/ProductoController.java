90a8cef819fee987074301672a2fd8a9719a79d6
andres <andres06energy@hotmail.com>
2021-12-12 19:43:29 -0300

    updated controller

--- src/main/java/com/api/productos/mypackages/controllers/ProductoController.java
+++ src/main/java/com/api/productos/mypackages/controllers/ProductoController.java
@@ -1,12 +1,32 @@
 package com.api.productos.mypackages.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.productos.mypackages.entities.Producto;
+import com.api.productos.mypackages.service.ProductoService;
 
 @RestController
 @RequestMapping("/v1")
 public class ProductoController {
 	
+	//INYECCION DEL SERVICE
+	@Autowired
+	@Qualifier("ProductoService")
+	ProductoService productoService;
+	
+	//METODO PUT
+	@PutMapping("/productos")
+	public boolean agregarProducto(@RequestBody @Validated Producto producto) {
+		return productoService.agregarProducto(producto);
+	}
+	
 
 
 }
+ 
--- src/main/java/com/api/productos/mypackages/entities/Producto.java
+++ src/main/java/com/api/productos/mypackages/entities/Producto.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Table(name="Producto")
+@Table(name="productos")
 @Entity
 public class Producto implements Serializable{
 	
